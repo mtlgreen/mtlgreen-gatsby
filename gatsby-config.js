70e74b1baff0a327058f062f1d64efb1874920c9
Paul Cuciureanu <paulcu@gmail.com>
2017-09-06 22:03:54 -0400

    Initial gatsby static website

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: 'MtlGreen',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-images'
+        ]
+      }
+    }
+  ]
+}
---
+++ src/pages/06-09-2017-hello/index.md
@@ -0,0 +1,7 @@
+---
+path: "/green-up"
+date: "2017-09-06T17:12:33.962Z"
+title: "Green up!"
+---
+
+Oooooh-weeee, the first blog post!
